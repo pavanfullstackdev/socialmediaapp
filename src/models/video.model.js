a960b45bd98839982780f24aa59c2bbe75157e41
pavanfullstackdev <pavan.fullstack.dev.tech@gmail.com>
2025-01-24 23:29:24 +0530

    jwt token & bcrypt integrated with user & video modle schema

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String,  //cloundniry
+        required: true
+    },
+    thumbnail: {
+        type: String,   //cloundniry
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,    //cloundniry
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true })
+
+
+//aggrigation pipeline
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
