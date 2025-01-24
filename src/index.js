758035ed24e24f4895639123ea6361b24d76e071
pavanfullstackdev <pavan.fullstack.dev.tech@gmail.com>
2025-01-25 00:18:42 +0530

    Cloudinary & multer integrated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
+import express from "express";
 import connectDB from "../src/db/index.js";
+
+const app = express();
 
 dotenv.config({
     path: './env'
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+//universal for all projects
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRETE,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uplaoded successfull
+        console.log("File is uploaded on cloudinary", response.url)
+        return response
+    } catch (err) {
+        //how to remove the file if fiel is not uploaded then unlink it means delete it from local server
+        fs.unlinkSync(localFilePath) //remove the localy temprory file 
+        return null
+
+    }
+}
+
+export { uploadOnCloudinary }
